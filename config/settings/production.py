a5c055dfb2c38a56850d314148d9ab1f44e4ae22
Anrey-Yakovtsev <ayakovtsev@gmail.com>
2023-10-18 00:01:00 +0300

    Settings:base.py:restored WhiteNoise middleware
    restored WhiteNoise middleware

--- config/settings/production.py
+++ config/settings/production.py
@@ -69,7 +69,8 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        # "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 # MEDIA
