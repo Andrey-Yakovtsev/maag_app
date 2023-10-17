ae198662ddbe1f4244afd6aa970bc70f32d6acf0
Anrey-Yakovtsev <ayakovtsev@gmail.com>
2023-10-18 00:13:12 +0300

    Settings:base.py:Allowed_hosts restored WhiteNoise middleware

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,9 +14,11 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list(
-    "DJANGO_ALLOWED_HOSTS", default=["maag-om.space", "82.146.38.136"]
-)
+# ALLOWED_HOSTS = env.list(
+#     "DJANGO_ALLOWED_HOSTS", default=["maag-om.space", "82.146.38.136"]
+# )
+ALLOWED_HOSTS = ["maag-om.space", "82.146.38.136"]
+
 
 # DATABASES
 # ------------------------------------------------------------------------------
