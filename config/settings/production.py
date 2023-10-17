7f4c497096dca5c424be0cc13510ae2e31ce5e73
Anrey-Yakovtsev <ayakovtsev@gmail.com>
2023-10-17 23:17:08 +0300

    WhiteNoise:middleware: remove for buildup project
    Removed Whitenoise to avoid deploy error

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,9 +14,7 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list(
-    "DJANGO_ALLOWED_HOSTS", default=["maag-om.space", "82.146.38.136"]
-)
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=[".maag-om.space"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
